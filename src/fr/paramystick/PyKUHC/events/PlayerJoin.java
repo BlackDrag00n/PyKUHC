f280e7b288c71dc60017f8e2541b626a0d953150
mms92 <martin-manche@hotmail.fr>
2015-03-01 12:18:50 +0100

    modif de la variable joueur vers PluginPyKUCH

--- src/fr/paramystick/PyKUHC/events/PlayerJoin.java
+++ src/fr/paramystick/PyKUHC/events/PlayerJoin.java
@@ -8,15 +8,15 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import fr.paramystick.PyKUHC.PyKUHC;
 
 public class PlayerJoin implements Listener {
-	private PyKUHC joueur; // On crée la variable qui va etre utiliser dans cette classe seulement (PRIVATE)
+	private PyKUHC pluginPyKUHC; // On crée la variable qui va etre utiliser dans cette classe seulement (PRIVATE)
 	
-	public PlayerJoin(PyKUHC joueur) { // On crée le constructor
-		this.joueur = joueur;
+	public PlayerJoin(PyKUHC pluginPyKUHC) { // On crée le constructor
+		this.pluginPyKUHC = pluginPyKUHC;
 	}
 	
 	@EventHandler
 	public void playerJoin(PlayerJoinEvent evenement) { // Si l'event est un JOIN serveur
-		evenement.setJoinMessage(ChatColor.AQUA + "[" + joueur.nomPlugin + "] " + ChatColor.GOLD + evenement.getPlayer().getName() + ChatColor.GREEN + " a rejoint la session !"); // On envoie un message a tout le monde pour prevenir qu'un joueur a rejoint le serveur
-		evenement.getPlayer().sendMessage(ChatColor.AQUA + "[" + joueur.nomPlugin + "] " + ChatColor.GREEN + "Bienvenu, Former les equipes et preparez-vous au combat !"); // On envoie un message au joueur qui rejoint
+		evenement.setJoinMessage(ChatColor.AQUA + "[" + pluginPyKUHC.nomPlugin + "] " + ChatColor.GOLD + evenement.getPlayer().getName() + ChatColor.GREEN + " a rejoint la session !"); // On envoie un message a tout le monde pour prevenir qu'un joueur a rejoint le serveur
+		evenement.getPlayer().sendMessage(ChatColor.AQUA + "[" + pluginPyKUHC.nomPlugin + "] " + ChatColor.GREEN + "Bienvenu, Former les equipes et preparez-vous au combat !"); // On envoie un message au joueur qui rejoint
 	}
 }
--- src/fr/paramystick/PyKUHC/events/PlayerQuit.java
+++ src/fr/paramystick/PyKUHC/events/PlayerQuit.java
@@ -8,14 +8,14 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import fr.paramystick.PyKUHC.PyKUHC;
 
 public class PlayerQuit implements Listener {
-	private PyKUHC joueur; // On crée la variable qui va etre utiliser dans cette classe seulement (PRIVATE)
+	private PyKUHC pluginPyKUHC; // On crée la variable qui va etre utiliser dans cette classe seulement (PRIVATE)
 	
-	public PlayerQuit(PyKUHC joueur) { // On crée le constructor
-		this.joueur = joueur;
+	public PlayerQuit(PyKUHC pluginPyKUHC) { // On crée le constructor
+		this.pluginPyKUHC = pluginPyKUHC;
 	}
 	
 	@EventHandler
 	public void playerQuit(PlayerQuitEvent evenement) {
-		evenement.setQuitMessage(ChatColor.AQUA + "[" + joueur.nomPlugin + "] " + ChatColor.GOLD + evenement.getPlayer().getName() + ChatColor.GREEN + " a fuis la session !"); // On envoie un message a tout le monde pour prevenir qu'un joueur a quitter le serveur
+		evenement.setQuitMessage(ChatColor.AQUA + "[" + pluginPyKUHC.nomPlugin + "] " + ChatColor.GOLD + evenement.getPlayer().getName() + ChatColor.GREEN + " a fuis la session !"); // On envoie un message a tout le monde pour prevenir qu'un joueur a quitter le serveur
 	}
 }
